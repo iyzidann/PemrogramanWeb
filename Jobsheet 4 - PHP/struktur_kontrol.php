550b015bf8050be5a55f78ce859305bb2c5c58f8
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-03-11 14:36:46 +0700

    modified:   Jobsheet 4 - PHP/struktur_kontrol.php

--- Jobsheet 4 - PHP/struktur_kontrol.php
+++ Jobsheet 4 - PHP/struktur_kontrol.php
@@ -55,6 +55,62 @@ foreach ($nilaisiswa as $nilai) {
     }
     echo "Nilai: $nilai(Lulus) <br>";
 }
+echo "<br><br>";
 
 
+echo "Soal Cerita no 4.6 <br><br>";
+
+$nilai_siswa = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+
+sort($nilai_siswa);
+
+$nilai_siswa = array_slice($nilai_siswa, 2, -2);
+$total_nilai = array_sum($nilai_siswa);
+$jumlah_siswa = count($nilai_siswa);
+$rata_rata = $total_nilai / $jumlah_siswa;
+
+echo "Daftar nilai siswa: " . implode(", ", $nilai_siswa) . "\n";
+echo "<br>";
+echo "Total nilai setelah mengabaikan 2 nilai tertinggi dan 2 nilai terendah: " . $total_nilai . "\n";
+echo "<br>";
+echo "Jumlah siswa: " . $jumlah_siswa . "\n";
+echo "<br>";
+echo "Rata-rata nilai: " . $rata_rata . "\n";
+echo "<br><br>";
+
+
+echo "Soal Cerita no 4.7 <br><br>";
+
+$harga_produk = 120000;
+$batas_diskon = 100000;
+$presentase_diskon = 20;
+
+if ($harga_produk > $batas_diskon) {
+    $diskon = ($presentase_diskon / 100) * $harga_produk;
+} else {
+    $diskon =  0;
+}
+
+$total_harga = $harga_produk -$diskon;
+
+echo "Harga produk: Rp " . number_format($harga_produk, 0, ',', '.') . "\n";
+echo "<br>";
+echo "Diskon (" . $presentase_diskon . "%): Rp " . number_format($diskon, 0, ',', '.') . "\n";
+echo "<br>";
+echo "Total harga setelah diskon: Rp " . number_format($total_harga, 0, ',', '.') . "\n";
+echo "<br><br>";
+
+echo "Soal Cerita no 4.8 <br><br>";
+
+$skor_game = [100, 200, 250, 300];
+
+$total_skor = 0;
+
+foreach ($skor_game as $angka) {
+    $total_skor += $angka;
+}
+echo "Total skor pemain adalah $total_skor <br>";
+echo "Apakah pemain mendapat hadiah tambahan? ";
+$hadiah = ($total_skor > 500)? "Ya" : "Tidak";
+echo $hadiah;
 ?>
