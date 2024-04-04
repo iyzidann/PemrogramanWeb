5afcea19486aa4588cf48c69ca10cd0e1fe57b15
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-04-05 05:02:36 +0700

    new file:   Jobsheet 8 - PHP Form Upload/documents/GL00 - Rencana Pengembangan PL.doc     new file:   Jobsheet 8 - PHP Form Upload/documents/GL01 - SKPL .doc 	new file:   Jobsheet 8 - PHP Form Upload/documents/GL02-Rancangan Perangkat Lunak.doc 	new file:   Jobsheet 8 - PHP Form Upload/documents/PBL - Sistem Survey Kepuasan.pdf 	new file:   Jobsheet 8 - PHP Form Upload/documents/P_WEB.txt 	new file:   Jobsheet 8 - PHP Form Upload/form_multiupload.php 	new file:   Jobsheet 8 - PHP Form Upload/form_upload.php 	new file:   Jobsheet 8 - PHP Form Upload/form_upload_ajax.php 	new file:   Jobsheet 8 - PHP Form Upload/proses_upload.php 	new file:   Jobsheet 8 - PHP Form Upload/upload.css 	new file:   Jobsheet 8 - PHP Form Upload/upload.js 	new file:   Jobsheet 8 - PHP Form Upload/upload.php 	new file:   Jobsheet 8 - PHP Form Upload/upload_ajax.php 	new file:   Jobsheet 8 - PHP Form Upload/uploads/foto.jpg

---
+++ Jobsheet 8 - PHP Form Upload/upload.php
@@ -0,0 +1,22 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $targetDirectory = "documents/"; // Direktori tujuan untuk menyimpan file
+        $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        $allowedExtensions = array("txt", "pdf", "doc", "docx");
+
+
+        $maxFileSize = 10 * 1024 * 1024;
+
+        if (in_array($fileType, $allowedExtensions)  && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+            if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+                echo "Dokumen berhasil diunggah";
+            } else {
+                echo "Gagal mengunggah dokumen.";
+            }
+        } else {
+            echo "Jenis dokumen tidak valid atau melebihi ukuran meksimum yang diizinkan.";
+        }
+    }
+?>
